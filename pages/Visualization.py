17534f90ec552404dddec5c3558a693b2d6bba6c
Suzuki Shosuke <begonia.sp.13@gmail.com>
2022-12-21 14:47:02 +0900

    add some comments to visualization.py

--- pages/Visualization.py
+++ pages/Visualization.py
@@ -7,9 +7,10 @@ from pages import Analysis
 
 def make_hist(dataset,dataset_column):
 
-    #visualize 1 column data
+    # visualize 1 column data
     if dataset[dataset_column].dtype != object:
-    
+        
+        # plot histgram and kde
         dataset[dataset_column].plot.hist(alpha=0.5,legend=True)
         dataset[dataset_column].plot.kde(alpha=0.5,legend=True, secondary_y=True)
         
@@ -17,8 +18,11 @@ def make_hist(dataset,dataset_column):
 
 
 def select_column(dataset):
-    #select column you want to visualize
+    
+    # select column you want to visualize
     st.caption('Select a column you want to visualize.')
+    
+    # select box : select a dataset column
     dataset_column = st.selectbox('Select a Column',dataset.columns.values,label_visibility="hidden")
     st.write(dataset_column)
     
@@ -29,16 +33,20 @@ def main():
     ui_analysis()
     dataset = Analysis.upload()
     
+    # check csv file uploaded
     if dataset is not None:
         st.markdown('### Descriptive Statistics')
+        
+        # make descriptive statistics table
         dataset_statics = dataset.describe().to_csv()
         
+        # download descriptive statistic csv file
         st.download_button( "Press to Download",
                             dataset_statics,
                             "dataset_statics.csv",
                             "text/csv",
                             key='download-csv' )
-        
+        # view descriptive statistics table
         st.write(dataset.describe())
         
         st.markdown('### Histgram and kernel density estimation')
@@ -46,9 +54,6 @@ def main():
         
         make_hist(dataset,dataset_column)
         
-    
-        
-    
 
 if __name__ == '__main__':
     main()
