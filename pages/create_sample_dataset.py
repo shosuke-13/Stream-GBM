a23a3754c87258e2a8f1071651c373824a5568d3
Suzuki Shosuke <84508762+shosuke-13@users.noreply.github.com>
2023-10-18 19:39:08 +0900

    Update create_sample_dataset.py

--- pages/create_sample_dataset.py
+++ pages/create_sample_dataset.py
@@ -3,9 +3,7 @@ import pandas as pd
 import numpy as np
 from main import ui_analysis
 
-
 st.set_page_config(layout="centered", page_icon=":shark:")
-
 
 def main():
     ui_analysis()
@@ -15,11 +13,9 @@ def main():
     
     st.markdown("#### Slide bar to select number of rows and columns")
 
-    # User input for number of rows and columns
     num_rows = st.slider("Number of Rows", min_value=1, max_value=1000, value=100)
     num_columns = st.slider("Number of Columns", min_value=2, max_value=10, value=5)
 
-    # Generate dummy data based on user input
     data = pd.DataFrame(np.random.randn(num_rows, num_columns), columns=[f"Feature {i+1}" for i in range(num_columns)])
     data["Target"] = data.sum(axis=1) + np.random.normal(0, 1, num_rows)
 
